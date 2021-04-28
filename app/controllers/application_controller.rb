7eadb702ee9767f58c0386d09c3006945e5139a6
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-04-28 20:11:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    
+    if  params[:username].empty? || params[:password].empty? 
+
+    	 @error= "User Name and Password can't be blank"
+
+    	 erb :'users/login'
+
+    elsif  @user=User.find_by(username: params[:username]) 
+
+             session[:user_id] = @user.id
+             redirect '/recipes'
+     else
+      
+     	@error ="Account not found "
+     	  erb :'users/login'
+
 
   end
 
